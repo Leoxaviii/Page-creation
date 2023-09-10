0bb8098da8fa88c52cc17fc21deabafea5c5fe48
Leoxaviii <132223441+Leoxaviii@users.noreply.github.com>
2023-09-10 21:14:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let time = document.getElementById ("currentUTCTime");
-setInterval(() =>{
-let d = new Date();
-time.innerHTML = d.toLocaleTimeString();
-},1000)
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        function updateClock() {
+            const currentDate = new Date();
+            const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+            const utcTime = currentDate.getTime(); // Get time in milliseconds
+
+            document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `${dayOfWeek}`;
+            document.querySelector('[data-testid="currentUTCTime"]').textContent = `${utcTime}`;
+  }
+        setInterval(updateClock, 1000);
+        updateClock();
+
